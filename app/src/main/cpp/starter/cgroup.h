01fc3458c772ab686587e9f75c4c355d241a7363
firesahc <firesahc@gmail.com>
2026-05-28 00:50:15 +0800

    chore: 清理 starter/、module.gradle、native_init

--- app/src/main/cpp/starter/cgroup.h
+++
@@ -1,9 +0,0 @@
-#ifndef CGROUP_H
-#define CGROUP_H
-
-namespace cgroup {
-    int get_cgroup(int pid, int* cuid, int *cpid);
-    int switch_cgroup(int pid, int cuid, int cpid);
-}
-
-#endif // CGROUP_H
